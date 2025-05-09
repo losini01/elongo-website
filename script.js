de402caeef251d735c217e9c9235236da22d41b4
losini01 <losini1@yahoo.fr>
2025-05-09 06:03:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// script.js
+// Exemple d'interaction simple : affichage d'un message de bienvenue
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('Site chargé avec succès !');
+});
